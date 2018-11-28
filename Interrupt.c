d353b8e919f455bd777061947a32c972fcee1e33
Leconte Vincent <vicente50130@gmail.com>
2018-11-28 11:34:26 +0100

    Interrupt

---
+++ Interrupt.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(INT0_vect){
+    //Faire le bidule
+    compteur++;
+}
+
+int set_interrupt(){
+
+    //etre sûr que TWEN de TWCR est à 0
+    DDRD &= ~(1<<DDD0); //Active interrupt 0 du PORTD
+    
+    EICRA |= (1<<ISCO1)|(1<<ISCO0); //Active le mode détection de fronts montants du INT0
+
+    EIMSK |= (1<<INT0); //Met le bit correspondant à l'interruption 0 sur 1 dans le EIMSK
+
+    SREG |= (1<<I); //Met le bit à 1 du global interrupt du status register, permet d'activer les interruptions
+
+}
+
+int main(){
+
+    int compteur_precedent(0);
+    int compteur(0);
+    set_interrupt();
+
+    while(True){
+        if(compteur_precedent!=compteur)
+        {
+            USART_Transmit(compteur);
+            compteur_precedent=compteur;
+        }
+    }
+    
+
+}
