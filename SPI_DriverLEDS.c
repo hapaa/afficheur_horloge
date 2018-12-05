2fea74a2f1c319b68fbcc6d4918e2365e7e3b235
Hamza Ezzarouali El Boudri <hapaa@hotmail.fr>
2018-12-05 11:52:09 +0100

    Working on LEDs

--- SPI_DriverLEDS.c
+++ SPI_DriverLEDS.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+
 void SPI_MasterInit(void)
 {
 /* Set MOSI and SCK output, all others input */
@@ -8,6 +9,7 @@ DDRB = (1<<DDB2)|(1<<DDB1);
 /* Enable SPI, Master, set clock rate fck/16 */
 SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
+
 
 void SPI_MasterTransmit(char cData)
 {
@@ -17,11 +19,22 @@ SPDR = cData;
 while(!(SPSR & (1<<SPIF)));
 }
 
+
 void Test_LED(void)
 {
     uint8_t value = 0;
     SPI_MasterTransmit(value);
-    value |= (1<<5)|(1<<6);
+    value = 0xff;
     SPI_MasterTransmit(value);
+}
 
+
+int main()
+{
+    SPI_MasterInit();
+    while (1) {
+        Test_LED();
+    }
+
+    return 0;
 }
