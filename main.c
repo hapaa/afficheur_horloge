2fb783ff69a4771b23ae58bd0fae4c9e5013fffe
Leconte Vincent <vicente50130@gmail.com>
2018-12-05 09:56:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,32 +6,34 @@
 #define BAUD 38400
 #define MYUBRR F_CPU/16/BAUD-1
 
+//DDRE |= (1<<DDE2); METTRE PE2 EN OUTPUT, SUPPOSEMENT EQUIVALENT A METTRE XCKO SUR PE2
+//PORTE &= ~(1<<PORTE2); FORCER INPUT XCK0 A 0, SUPPOSEMENT EMPECHER MODE CONFIG
 
 void USART_Init (unsigned int ubrr)
 {
 /* Set baud rate */
-UBRR1H = (unsigned char)(ubrr>>8);
-UBRR1L = (unsigned char)ubrr;
+UBRR0H = (unsigned char)(ubrr>>8);
+UBRR0L = (unsigned char)ubrr;
 /* Enable receiver and transmitter */
-UCSR1B = (1<<RXEN)|(1<<TXEN);
+UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 /* Set frame format: 8data, 2stop bit */
-UCSR1C = (1<<USBS)|(3<<UCSZ1);
+UCSR0C = (1<<USBS0)|(1<<UCSZ00)|(1<<UCSZ01);
 }
 
 
 void USART_Transmit( unsigned char data ) {
 /* Wait for empty transmit buffer */
-while ( !( UCSR1A & (1<<UDRE1)) );
+while ( !( UCSR0A & (1<<UDRE0)) );
 /* Put data into buffer, sends the data */
-UDR1 = data;
+UDR0 = data;
 }
 
 
 unsigned char USART_Receive( void ) {
 /* Wait for data to be received */
-while ( !(UCSR1A & (1<<RXC1)) );
+while ( !(UCSR0A & (1<<RXC0)) );
 /* Get and return received data from buffer */
-return UDR1;
+return UDR0;
 }
 
 
