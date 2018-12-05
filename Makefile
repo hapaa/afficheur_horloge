9acb2a08bb871d2cebe77ca2fb69d88106c88515
Hamza Ezzarouali El Boudri <hapaa@hotmail.fr>
2018-12-05 08:54:20 +0100

    start session

--- Makefile
+++ Makefile
@@ -1,14 +1,12 @@
 
-.PHONY: all install
-all: install
 
 
-install: prog.hex
-	avrdude -v -p atmega128 -c jtagmkI -P /dev/ttyUSB0 -D -U "flash:w:prog.hex:i"
+all: prog.hex
+	avrdude -V -F -p atmega128 -c jtagmkI -P /dev/ttyUSB0 -D -U "flash:w:prog.hex:i"
 
 
 prog.hex: main.c
-	avr-gcc -mmcu=atmega128 -O2 main.c -o prog.elf
+	avr-gcc -Werror -Wall -DF_CPU=13000000 -mmcu=atmega128 -O2 main.c -o prog.elf
 	avr-objcopy -j .text -O ihex prog.elf prog.hex
 
 clean:
--- main.c
+++ main.c
@@ -3,43 +3,43 @@
 #include <util/delay.h>
 
 
-#define F_CPU 13000000
-#define BAUD 9600
-#define MYUBRR F_CPU/8/BAUD-1
+#define BAUD 38400
+#define MYUBRR F_CPU/16/BAUD-1
 
 
 void USART_Init (unsigned int ubrr)
 {
-  /* Set baud rate */
-  UBRR0H = (unsigned char)(ubrr>>8);
-  UBRR0L = (unsigned char)ubrr;
-  /* Enable receiver and transmitter */
-  UCSR0B = (1<<RXEN)|(1<<TXEN);
-  /* Set frame format: 8data, 2stop bit */
-  UCSR0C = (1<<USBS)|(3<<UCSZ0);
+/* Set baud rate */
+UBRR1H = (unsigned char)(ubrr>>8);
+UBRR1L = (unsigned char)ubrr;
+/* Enable receiver and transmitter */
+UCSR1B = (1<<RXEN)|(1<<TXEN);
+/* Set frame format: 8data, 2stop bit */
+UCSR1C = (1<<USBS)|(3<<UCSZ1);
 }
 
 
 void USART_Transmit( unsigned char data ) {
-  /* Wait for empty transmit buffer */
-  while ( !( UCSR0A & (1<<UDRE0)) );
-  /* Put data into buffer, sends the data */
-  UDR0 = data;
+/* Wait for empty transmit buffer */
+while ( !( UCSR1A & (1<<UDRE1)) );
+/* Put data into buffer, sends the data */
+UDR1 = data;
 }
 
 
 unsigned char USART_Receive( void ) {
-  /* Wait for data to be received */
-  while ( !(UCSR0A & (1<<RXC0)) );
-  /* Get and return received data from buffer */
-  return UDR0;
+/* Wait for data to be received */
+while ( !(UCSR1A & (1<<RXC1)) );
+/* Get and return received data from buffer */
+return UDR1;
 }
 
 
-int main(){
-    
-  USART_Init(MYUBRR);
-  while (1){
-      USART_Transmit(USART_Receive()+1);
+int main()
+{
+
+USART_Init(MYUBRR);
+while (1){
+USART_Transmit(USART_Receive()+1);
   }
 }
